6e6a998a315461d09f80e2710e4949d2245f7aca
aidanliddiard <aidanliddiard@gmail.com>
2021-12-07 15:48:14 -0800

    starting themes

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 //NAME
 // get the input form control
 const nameInput = document.getElementById('name-input');
-
-// get the span for the birthday name
 const birthdayName = document.getElementById('birthday-name');
-
-// add the event listener for input changing
-// 1. name of event (string)
-// 2. function to "call us back" when the event happens
 nameInput.addEventListener('input', () => {
-    console.log('input changed', nameInput.value);
-birthdayName.textContent = nameInput.value;
+    birthdayName.textContent = nameInput.value;
 })
-    //when it changes, update the value of the span
 
     // Image
 const photoSelect = document.getElementById('photo-select');
 const chosenPhoto = document.getElementById('chosen-image');
 photoSelect.addEventListener('change', () => {
-    console.log('photo changed', photoSelect.value);
     chosenPhoto.src = photoSelect.value;
+});
+
+// Theme
+const themeSelect = document.getElementById('photo-select');
+const card = document.getElementById('card');
+themeSelect.addEventListener('change', () => {
+  card.classList.value = '';
+  card.classList.add(themeSelect.value);
 });
 
 // Message
@@ -37,7 +36,7 @@ fromInput.addEventListener('input', () => {
 });
 
 //Export
-const card = document.getElementById('card');
+//const card = document.getElementById('card');
 const exportButton = document.getElementById('export-button');
 exportButton.addEventListener('click', async () => {
   const dataUrl = await domtoimage.toPng(card);
