abb3f05a379ad0541c452807f2350c94576f5037
aidanliddiard <aidanliddiard@gmail.com>
2021-12-07 13:04:52 -0800

    changing birthday name

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+//NAME
 // get the input form control
 const nameInput = document.getElementById('name-input');
 
-// get the span for the band name
-const bandName = document.getElementById('band-name');
+// get the span for the birthday name
+const birthdayName = document.getElementById('birthday-name');
 
 // add the event listener for input changing
 // 1. name of event (string)
 // 2. function to "call us back" when the event happens
 nameInput.addEventListener('input', () => {
-bandName.textContent = nameInput.value;
+    console.log('input changed', nameInput.value);
+birthdayName.textContent = nameInput.value;
 })
     //when it changes, update the value of the span
